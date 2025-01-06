36bf4d76e050dd3b0921328a64f42e35b10af8a0
carmelo <django.carmelo@gmail.com>
2025-01-06 19:57:15 +0900

    Credentials Sign In Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -33,7 +34,10 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* FORM HERE */}</CardContent>
+        <CardContent className="space-y-4">
+          {/* FORM HERE */}
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );
