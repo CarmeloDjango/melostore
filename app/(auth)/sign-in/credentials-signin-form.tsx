27d5ca137876cb15967e4bd1b97608cf7128cbcf
carmelo <django.carmelo@gmail.com>
2025-01-06 20:17:12 +0900

    Hook Up Sign In Form

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -5,10 +5,29 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { signInWithCredentials } from "@/lib/actions/user.action";
+import { useFormStatus } from "react-dom";
+// react19에서 useActionState로 바뀜 (원래 useFormState)
+import { useActionState } from "react";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -34,10 +53,13 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign In
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?
           <Link href="/sign-up" target="_self" className="ml-1 hover:underline">
