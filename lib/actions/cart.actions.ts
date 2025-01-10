b5b8e7eb8d317e55d2669408af522054d3162122
carmelo <django.carmelo@gmail.com>
2025-01-10 14:36:12 +0900

    Add to Cart Component

---
+++ lib/actions/cart.actions.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { CartItem } from "@/types";
+
+export async function addItemToCart(data: CartItem) {
+  return {
+    success: true,
+    message: `${data.name} added to cart`,
+  };
+}
